6eb81ea049b15d66501bdbd5fd23f791e9502f83
Elina <elina@MacBook-Pro-Elina.local>
2025-11-14 22:19:46 +0200

    develop

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import cn from 'classnames';
-import { Link, NavLink, useLocation } from 'react-router-dom';
+import classNames from 'classnames';
+import { Link, useLocation } from 'react-router-dom';
 
-export const Navbar: React.FC = () => {
-  const { pathname, hash } = useLocation();
+export const Navbar = () => {
+  const { pathname } = useLocation();
 
   return (
     <nav
@@ -14,19 +14,17 @@ export const Navbar: React.FC = () => {
     >
       <div className="container">
         <div className="navbar-brand">
-          <NavLink
+          <Link
+            className={classNames('navbar-item', {
+              'has-background-grey-lighter': pathname === '/',
+            })}
             to="/"
-            className={({ isActive }: { isActive: boolean }) =>
-              cn('navbar-item', {
-                'has-background-grey-lighter': isActive || pathname === '/' || hash === '#/',
-              })
-            }
           >
             Home
-          </NavLink>
+          </Link>
 
           <Link
-            className={cn('navbar-item', {
+            className={classNames('navbar-item', {
               'has-background-grey-lighter': pathname.startsWith('/people'),
             })}
             to="/people"
